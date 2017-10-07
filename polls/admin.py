a78ede06b13e93bb3f00c757b172446a9eae758c
notechus <seba0713@gmail.com>
2017-10-07 17:27:23 +0200

    final touches

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,34 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Question,Choice
+from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_per_page = 25
+    date_hierarchy = 'pub_date'
+    empty_value_display = '-empty-'
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question', {'fields': ['question']}),
+        ('Choice', {'fields': ['choice_text']})
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
